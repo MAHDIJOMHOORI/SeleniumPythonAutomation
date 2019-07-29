85c77b5309b713bd7504aea0eb18314db08a2cb7
AdityaSingh17 <asaditya140@gmail.com>
2019-07-29 19:41:39 +0530

    Added new test account.

--- sendemail.py
+++ sendemail.py
@@ -13,14 +13,16 @@ browser.get('https://accounts.google.com/signin/v2/identifier?continue=https%3A%
 
 # em = browser.find_element_by_id("identifierId")
 em = wait.until(EC.element_to_be_clickable((By.ID, 'identifierId')))    #As soon as the element is visible in DOM, execute the commands.
-em.send_keys('ss.sakshisingh02@gmail.com')
+em.send_keys('testme7714@gmail.com')
+time.sleep(1)
 em.send_keys(Keys.RETURN)
 
 # time.sleep(2)
 
 # passw = browser.find_element_by_name("password")
-passw = wait.until(EC.element_to_be_clickable((By.NAME, 'password')))
-passw.send_keys("ishuaditya02")
+passw = wait.until(EC.element_to_be_clickable((By.NAME,'password')))
+passw.send_keys("Aditya123")
+time.sleep(1)
 passw.send_keys(Keys.RETURN)
 
 # comp = browser.find_element_by_xpath("/html/body/div[7]/div[3]/div/div[2]/div[1]/div[1]/div[1]/div/div/div/div[1]/div/div").click()
@@ -36,8 +38,8 @@ sub = wait.until(EC.element_to_be_clickable((By.NAME, 'subjectbox')))
 sub.send_keys('HI')
 time.sleep(1)
 
-body = browser.find_element_by_xpath('//*[@id=":bt"]')
-body = wait.until(EC.element_to_be_clickable((By.XPATH, '//*[@id=":bt"]')))
+# body = browser.find_element_by_xpath('//*[@id=":97"]')
+body = wait.until(EC.element_to_be_clickable((By.XPATH, '//*[@id=":97"]')))
 body.send_keys("hey addy!")
 time.sleep(1)
 
